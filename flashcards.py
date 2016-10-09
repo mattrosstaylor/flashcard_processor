4b28885f2904be4266e2dc9ccc6cff2e03d26e6c
Matt R Taylor <matt.ross.taylor@gmail.com>
2016-10-09 15:34:17 +0800

    minimum set cover implemented

--- flashcards.py
+++ flashcards.py
@@ -1,104 +1,66 @@
 # -*- coding: utf-8 -*-
-
 #!/bin/python
 
-import csv, codecs, cStringIO
+from unicode_csv import UnicodeReader 
 import operator
 import re
-
-class UTF8Recoder:
-
-    def __init__(self, f, encoding):
-        self.reader = codecs.getreader(encoding)(f)
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        return self.reader.next().encode("utf-8")
-
-class UnicodeReader:
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        f = UTF8Recoder(f, encoding)
-        self.reader = csv.reader(f, dialect=dialect, delimiter='\t', **kwds)
-
-    def next(self):
-        row = self.reader.next()
-        return [unicode(s, "utf-8") for s in row]
-
-    def __iter__(self):
-        return self
-
-class UnicodeWriter:
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        data = self.encoder.encode(data)
-        self.stream.write(data)
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
+import random
 
 class Entry:
-    def __init__(self, cat, sc, py, en=None):
-        self.sc = sc
-        self.py = py
-        self.en = en
-        self.cat = cat
-
-        self.pypy = py.encode("ascii", "replace").replace("?", "v")
-        self.count = len(sc)
+	def __init__(self, category, text, pinyin, english=None):
+		self.text = text
+		self.pinyin = pinyin
+		self.english = english
+		self.category = category
+		self.count = len(text)
+		self.related = set()
     
-    def __str__(self):
-        return self.pypy
+	def __repr__(self):
+		return self.text.encode("utf-8")
 
-    def __unicode__(self):
-    	return self.sc +" " +self.py +" " +self.cat
+	def __unicode__(self):
+		return self.sc +" " +self.py +" " +self.category
 
-lines = UnicodeReader(open("flashcard.txt"))
 
-entries = []
-category_name = None
+def read_entries_from_csv(path):
 
-for r in lines:
-	if r:
-		if "// " in r[0]:
-			category_name = r[0]
-		else:
-			if u'\u2026' in r[0]: # search for elipsis
-				sctokens = r[0].split(u'\u2026')
-				pytokens = r[1].split(u'\u2026')
+	lines = UnicodeReader(open(path))
 
-				for i, t in enumerate(sctokens):
-					if t:
-						entry = Entry(category_name, t, pytokens[i])
-						entries.append(entry)
+	entries = []
+	category_name = None
+
+	for r in lines:
+		if r:
+			if "// " in r[0]:
+				category_name = r[0]
 			else:
-				if len(r) == 3:
-					entry = Entry(category_name, r[0], r[1], r[2])
-					entries.append(entry)
+				if u'\u2026' in r[0]: # search for elipsis
+					text_tokens = r[0].split(u'\u2026')
+					pinyin_tokens = r[1].split(u'\u2026')
+
+					for i, t in enumerate(text_tokens):
+						if t:
+							entry = Entry(category_name, t, pinyin_tokens[i])
+							entries.append(entry)
 				else:
-					entry = Entry(category_name, r[0], r[1])
-					entries.append(entry)
+					if len(r) == 3:
+						entry = Entry(category_name, r[0], r[1], r[2])
+						entries.append(entry)
+					else:
+						entry = Entry(category_name, r[0], r[1])
+						entries.append(entry)
+
+	return entries
+
+entries = read_entries_from_csv("export.txt")
 
 words = []
 sentences = []
 
 for entry in entries:
-	if "Example" in entry.cat:
+	if "Example" in entry.category:
 		sentences.append(entry)
-	elif not "Learning" in entry.cat:
+	elif not "Learning" in entry.category:
 		words.append(entry)
 
 words.sort(key=operator.attrgetter('count'), reverse=True)
@@ -126,47 +88,92 @@ bad_chars = [
 	" "
 ]
 
+def increment_word(words, character):
+	if character in words:
+		words[character] += 1
+	else:
+		words[character] = 1
+
+def output_word_list(words):
+	it = sorted(words.items(), key=operator.itemgetter(1))
+	for sc in it:
+		print sc[0] +" " +str(words[sc[0]])
+
 unknown = dict()
 passed = []
 
 for s in sentences:
-	sc = s.sc
-	sc_printable = s.sc
+	example = s.text
+	example_printable = s.text
 
 	for w in words:
-		if w.sc in sc:
-			sc = sc.replace(w.sc,"")
-			sc_printable = sc_printable.replace(w.sc,u' '*w.count)
-			# print unicode(w)
+		if w.text in example:
+			example = example.replace(w.text,"")
+			example_printable = example_printable.replace(w.text, u' '*w.count)
 
 	for bc in bad_chars:
-		sc = sc.replace(bc,"")
-		sc_printable = sc_printable.replace(bc,"*")
+		example = example.replace(bc,"")
+		example_printable = example_printable.replace(bc,"*")
 	
-	sc_printable = re.sub(r'(.)\1+', r'\1', sc_printable) 
-	sc_printable = sc_printable.replace("*", " ").strip()
+	if example:
+		example_printable = re.sub(r'(\*)\1+', r'\1', example_printable) 
+		example_printable = example_printable.replace("*", " ").strip()
 
-
-	if sc:
-		#print unicode(s.sc)
-		#print sc_printable
-		#print ""
-
-		junk = sc_printable.split(' ')
+		junk = example_printable.split(' ')
 		for j in junk:
-			if j in unknown:
-				unknown[j] += 1
-			else:
-				unknown[j] = 1
+			increment_word(unknown, j)
 	else:
-		passed.append(s)	
+		passed.append(s)
 
-#it = sorted(unknown.items(), key=operator.itemgetter(1))
+known = dict()
 
-#for sc in it:
-#	print sc[0] +" " +str(unknown[sc[0]])
+for s in passed:
+	example = s.text
+
+	for w in words:
+		if w.text in example:
+			example = example.replace(w.text,"")
+			increment_word(known, w.text)
+			s.related.add(w)
+			w.related.add(s)
+"""
+	contains = "\t"
+	for x in s.related:
+		contains = contains +" " +x.text
+
+	print s.text + " " +str(len(s.related))
+"""
+
+covering = []
+
+while passed:
+	passed.sort(key=lambda x: x.count, reverse=False)
+	#passed.sort(key=lambda x: len(x.related))
+
+	#top = passed.pop()
+	top = passed[random.randint(0,len(passed)-1)]
+	passed.remove(top)
+
+	for s in passed:
+		for chars in top.related:
+			if chars in s.related:
+				s.related.remove(chars)
+
+		if not len(s.related):
+			passed.remove(s)
+
+	covering.append(top)
+
+
+print len(covering)
+
+#for w in words:
+#	if not w.text in known:
+#		print w.text
+
+#output_word_list(known)
 
 print "// Learning".encode('utf-8')
-for s in passed:
-	if s.sc and s.py and s.en:
-		print (s.sc +"\t" +s.py +"\t" +s.en).encode('utf-8')
+for s in covering:
+	if s.text and s.pinyin and s.english:
+		print (s.text +"\t" +s.pinyin +"\t" +s.english).encode('utf-8')
