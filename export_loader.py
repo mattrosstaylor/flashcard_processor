0cfa353e268b729d77555e32086e8c190e606cb5
Matt R Taylor <matt.ross.taylor@gmail.com>
2017-07-21 19:16:47 +0800

    currently reprogrammed to work with spoonfed vocab for anki. Commented code.

--- export_loader.py
+++ export_loader.py
@@ -6,11 +6,10 @@ import operator
 import re
 
 class Entry:
-	def __init__(self, category, text, pinyin, english=None):
+	def __init__(self, text, pinyin, english=None):
 		self.text = text
 		self.pinyin = pinyin
 		self.english = english
-		self.category = category
 		self.count = len(text)
 		self.related = set()
     
@@ -20,33 +19,35 @@ class Entry:
 	def __unicode__(self):
 		return self.sc +" " +self.py +" " +self.category
 
+	def __eq__(self, other):
+		return self.text == other.text
+
+	def __hash__(self):
+		return hash(self.text)
+
 def read_entries_from_csv(path):
 
 	lines = UnicodeReader(open(path))
 
 	entries = []
-	category_name = None
 
 	for r in lines:
 		if r:
-			if "// " in r[0]:
-				category_name = r[0]
-			else:
-				if u'…' in r[0]: # search for ellipsis
-					text_tokens = r[0].split(u'…')
-					pinyin_tokens = r[1].split(u'…')
+			if u'…' in r[0]: # search for ellipsis
+				text_tokens = r[0].split(u'…')
+				pinyin_tokens = r[1].split(u'…')
 
-					for i, t in enumerate(text_tokens):
-						if t:
-							entry = Entry(category_name, t, pinyin_tokens[i])
-							entries.append(entry)
+				for i, t in enumerate(text_tokens):
+					if t:
+						entry = Entry(t, pinyin_tokens[i])
+						entries.append(entry)
+			else:
+				if len(r) >= 3:
+					entry = Entry(r[0], r[1], r[2])
+					entries.append(entry)
 				else:
-					if len(r) == 3:
-						entry = Entry(category_name, r[0], r[1], r[2])
-						entries.append(entry)
-					else:
-						entry = Entry(category_name, r[0], r[1])
-						entries.append(entry)
+					entry = Entry(r[0], r[1])
+					entries.append(entry)
 
 	return entries
 
@@ -61,61 +62,80 @@ def output_word_list(words):
 	for sc in it:
 		print sc[0].text +" " +str(words[sc[0]])
 
-def load_export_file(path):
+def load_word_file(path):
+	return read_entries_from_csv(path)
+
+def load_export_file(path, words):
 	entries = read_entries_from_csv(path)
 
-	words = []
 	sentences = []
 
 	for entry in entries:
-		if "Example" in entry.category:
-			sentences.append(entry)
-		elif not "Learning" in entry.category:
-			words.append(entry)
-
-	words.sort(key=operator.attrgetter('count'), reverse=True)
+		sentences.append(Entry(entry.english, entry.pinyin, entry.text))
 
 	bad_chars = [
+		u"，" ,
 		u"、" , 
 		u"。" , 
 		u"；" ,
+		u"：" ,
 		u"—" ,
 		u"？" ,
 		u"（" ,
 		u"）" ,
+		u"《",
+		u"》" ,
 		u"→" , 
 		u"”" , 
 		u"“" ,
 		u"！" ,
 		"(" ,
 		")" ,
+		"<" ,
+		">" ,
 		"/" ,
 		"," , 
+		"." ,
 		"!" ,
 		";" ,
 		":" ,
+		"'" ,
 		"?" ,
+		"%" ,
+		"&" ,
 		" "
 	]
 
-	unknown_word_counts = dict()
+	words.sort(key=operator.attrgetter('count'), reverse=True)
+
+	# examples without unknown characters
 	passed = []
+
+	# unknown words mapped to number of entries with that word
+	unknown_word_counts = dict()
 
 	for s in sentences:
 		example = s.text
 		example_printable = s.text
 
+		# remove bad characters
+		for bc in bad_chars:
+			example = example.replace(bc,"")
+			example_printable = example_printable.replace(bc,"*")
+
+		# remove known words
 		for w in words:
 			if w.text in example:
 				example = example.replace(w.text,"")
 				example_printable = example_printable.replace(w.text, u' '*w.count)
 
-		for bc in bad_chars:
-			example = example.replace(bc,"")
-			example_printable = example_printable.replace(bc,"*")
-		
+		# minimise whitespace
+		example = re.sub(r'\w', '', example)
+		example_printable = re.sub(r'\w', '', example_printable)
+
+		# any example with characters remaining has not passed - records unknown chars
 		if example:
-			example_printable = re.sub(r'(\*)\1+', r'\1', example_printable) 
+			example_printable = re.sub(r'(\*)\1+', r'\1', example_printable)
 			example_printable = example_printable.replace("*", " ").strip()
 
 			junk = example_printable.split(' ')
@@ -124,4 +144,4 @@ def load_export_file(path):
 		else:
 			passed.append(s)
 
-	return (words, passed, unknown_word_counts)
+	return (passed, unknown_word_counts)
--- flashcards.py
+++ flashcards.py
@@ -6,12 +6,21 @@ from export_loader import *
 import operator
 import random
 
-minimum_desired_count = 3
-maximum_card_count = 10000
-(words, sentences, unknown) = load_export_file("export.txt")
+minimum_desired_count = 1000
+maximum_card_count = 30000
 
+words = load_word_file("vocab.txt")
+extras = load_word_file("vocab_extras.txt")
+
+words = words + extras
+
+(sentences, unknown) = load_export_file("spoonfed_sample.txt", words)
+
+#for w in unknown:
+#	print w
+
+# eliminate duplicates in sentence list (crudely)
 sentences = list(set(sentences))
-random.shuffle(sentences)
 
 known_word_counts = dict()
 
@@ -26,73 +35,89 @@ for s in sentences:
 			s.related.add(w)
 			w.related.add(s)
 
-covering = []
+# Output list of words with no matching sentences
+#for w in words:
+#	if not w.text in known_word_counts:
+#		print w.text
+
+# null initialise added_word_count dictionary
 added_word_counts = dict()
-added_sentences = set()
 
 for w in known_word_counts:
 	added_word_counts[w] = 0
 
+covering = []
+added_sentences = set()
+
 looping = True
 
 while looping:
-	if not len(covering) < maximum_card_count:
+	if len(covering) >= maximum_card_count or len(covering) >= len(sentences):
 		looping = False
 		break
 
-	it = sorted(added_word_counts.items(), key=operator.itemgetter(1))
+	# sort already added words incrementally by prevalence
+	it = sorted(added_word_counts.items(), key=operator.itemgetter(1)) #shuffle this list somehow...
 
+	# find the most common word's prevalence (i.e. the last word on this list)
 	maximum_prevalence = added_word_counts[it[len(it)-1][0]]
 
 	for w in it:
 		smallest_word = w[0]
 
+		# if you have already added all the examples from this word - continue
 		if known_word_counts[smallest_word] == added_word_counts[smallest_word]:
 			continue
 
+		# if the least common word has reached the desired count - we're done!
 		if not added_word_counts[smallest_word] < minimum_desired_count:
 			looping = False
 			break
 
-		
-		sentence = None
-		cost = 0
+		# find the best unadded sentence for this word - see below
+		best_sentence = None
+		best_sentence_score = 0
 
-		for s in smallest_word.related:
-			if s not in added_sentences:
+		# loop through sentences containing this word
+		for sentence in smallest_word.related:
+			if sentence in added_sentences:
+				continue
 				
-				total = 0
-				for ww in s.related:
-					total += 1 + maximum_prevalence - added_word_counts[ww]
-				
-				if total > cost:
-					sentence = s
-					cost = total
+			# base score is number of unique words
+			sentence_score = len(sentence.related) 
+			
+			# score bonus points for how rare each word is
+			for ww in sentence.related:
+				sentence_score += maximum_prevalence - added_word_counts[ww]
 
+			# check if best sentence for this word
+			if sentence_score > best_sentence_score:
+				best_sentence = sentence
+				best_sentence_score = sentence_score
+
+		# if no suitable sentences found - go to next smallest word
 		if not sentence:
 			continue
 
-		covering.append(sentence)
-		added_sentences.add(sentence)
+		# add this sentence - register as "added"
+		covering.append(best_sentence)
+		added_sentences.add(best_sentence)
 
-		for ww in sentence.related:
+		# update added_word_counts to include new sentence
+		for ww in best_sentence.related:
 			increment_word(added_word_counts, ww)
 		
-		break
+		break # back to main loop
 
-#for w in words:
-#	if not w.text in known_word_counts:
-#		print w.text
-
-#output_word_list(known_word_counts)
-
+# output sentence list
 print ("// Learning " +str(len(covering))).encode('utf-8')
 for s in covering:
 	if s.text and s.pinyin and s.english:
-		print (s.text +"\t" +s.pinyin +"\t" +s.english).encode('utf-8')
-		#print (s.text).encode('utf-8')
+		#print (s.text +"\t" +s.pinyin +"\t" +s.english).encode('utf-8')
+		#print (s.english +"\t" +s.pinyin +"\t" +s.text).encode('utf-8')
+		print (s.english +"\t" "learning").encode('utf-8')
 
-#print len(covering)
+#output_word_list(known_word_counts)
 
 #known_word_counts = dict()
 
